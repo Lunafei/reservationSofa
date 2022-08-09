b9930255aa5bbb49fe57de95e8ba6fd7b89475f0
lunafei <lunafei@gmail.com>
2022-08-09 20:27:10 +0900

    Initial Commit

---
+++ reservationSofa/reservationSofaApp.swift
@@ -0,0 +1,17 @@
+//
+//  reservationSofaApp.swift
+//  reservationSofa
+//
+//  Created by Ji Hoon Um on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct reservationSofaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
